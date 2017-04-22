9731f27a997d0a89068a08a6a72cd4f330fed6f3
AdelAdham <adeladham95@gmail.com>
2017-04-22 18:15:53 +0200

    Tested and Modified ALU, alu.do is provided

--- alu.vhd
+++ alu.vhd
@@ -20,10 +20,10 @@ COMPONENT my_nadder IS
          cout : OUT std_logic);
 END COMPONENT;
 
---aa,bb,not_b,x1,car1,x2,car2,y1,cary1,y2,cary2,x13,car13,x14,car14,car3,x3,x4,x5,car6,x6,x8,car8,x12,car12
-signal aa,bb,not_b,not_a,y1,y2,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,tmp_out :std_logic_vector(15  DOWNTO 0);
+--aa,bb,not_b,not_a,x1,car1,x2,car2,y1,cary1,y2,cary2,x13,car13,x14,car14,car3,x3,x4,x5,car6,x6,x8,car8,x12,car12,car10,x10
+signal aa,bb,not_b,not_a,y1,y2,x1,x2,x3,x4,x5,x6,x8,x10,x12,x13,x14,tmp_out :std_logic_vector(15  DOWNTO 0);
 signal flags_tmp :std_logic_vector(3  DOWNTO 0);
-signal not_cf,car1,car2,car3,car6,car7,car8,car9,car10,car11,car12,car13,car14,cary1,cary2: std_logic;
+signal not_cf,car1,car2,car3,car6,car8,car10,car12,car13,car14,cary1,cary2: std_logic;
 BEGIN
 
 --notcf: not_cf <= NOT flags_in(0);
@@ -48,10 +48,10 @@ decA:      my_nadder  port map (a,"1111111111111110",'1',y2,cary2);-- a-1
 logcar: car3 <=flags_in(0);	
 anding: x3 <= a and b; 
 oring: x4<= a or b;
-xoring: x5<= a xor b;
+--xoring: x5<= a xor b;
 
 SHR1:x8 <= std_logic_vector(shift_right(unsigned(a),to_integer(unsigned(b))));
-SHR2: car8 <= flags_in(0)
+SHR2: car8 <= flags_in(0);	
 --SHR2:x8(14 downto 0)<=b(15 downto 1);
 --SHR3: car8<= b(0);
 
@@ -61,13 +61,13 @@ SHL2: car12 <= flags_in(0);
 --SHL2: x12(15 downto 1)<=b(14 downto 0);
 --SHL3: car12<= b(15);
 
-RRC1: car10<=b(0);
+RRC1: car10<=a(0);
 RRC2: x10(15)<=flags_in(0);
-RRC3: x10(14 downto 0)<=b(15 downto 1);
+RRC3: x10(14 downto 0)<=a(15 downto 1);
 
 RLC1: x14(0)<=flags_in(0);
-RLC2: x14(15 downto 1)<=b(14 downto 0);
-RLC3: car14<=b(15);
+RLC2: x14(15 downto 1)<=a(14 downto 0);
+RLC3: car14<=a(15);
 
 --ASR1: x11(15)<=b(15);
 --ASR2: x11(14 downto 0)<=b(15 downto 1);
@@ -85,33 +85,39 @@ RLC3: car14<=b(15);
 out_alu: tmp_out <= aa when s="0000" --a
 else bb when s="0001" --b
 else not_a when s="1010" -- ~a
+else x6 when s="1011" -- -a
+
+else y1 when s="1100" --incA
+else y2 when s="1101" --decA
+
+else x1 when s="0010" --a+b
+else x2 when s="0011" --a-b
+
+else x3 when s="0100" --and
+else x4 when s="0101" --or 
+--else x5 when s="01010" --xor
+
+--else x7 when s="00111"
+else x8 when s="1001" --SHR
+else x12 when s="1000" --SHL
+--else x9 when s="10000" 
+else x10 when s="0111" --RRC
+else x14 when s="0110"; --RLC
+
+--else x11 when s="10010"
+--else x13 when s="10100"
+
 --else std_logic_vector(to_signed(to_integer(signed(a)) + 1 ,tmp_out'length)) WHEN s = "00010" --a+1 
 --else std_logic_vector(to_signed(to_integer(signed(a)) - 1 ,tmp_out'length)) WHEN s = "00011" --a-1
 --else std_logic_vector(to_signed(to_integer(signed(b)) + 1 ,tmp_out'length)) WHEN s = "01011" --b+1 
 --else std_logic_vector(to_signed(to_integer(signed(b)) - 1 ,tmp_out'length)) WHEN s = "01100" --b-1
-else x1 when s="0010" --a+b
-else x2 when s="0011" --a-b
-else x3 when s="0100" --and
-else x4 when s="0101" --or 
---else x5 when s="01010" --xor
-else x6 when s="1011" -- -a
---else x7 when s="00111"
-else y1 when s="1100" --incA
-else y2 when s="1101" --decA
 
-else x8 when s="1001" --SHR
---else x9 when s="10000" 
-else x10 when s="0111" --RRC
---else x11 when s="10010"
-else x12 when s="1000" --SHL
---else x13 when s="10100"
-else x14 when s="0110"; --RLC
 
 
 
 Cflag: flags_tmp(0)<= car1 when s="0010" --a+b
 else car2 when s="0011" --a-b
-else car3 when s="0100" or s="0101" --Logic operations
+else car3 when s="0100" or s="0101" or s="1010"--Logic operations
 else car6 when s="1011" -- -a
 else car12 when s="1000" --SHL
 else car8 when s="1001" --SHR
@@ -126,7 +132,6 @@ else cary2 when s="1101" --Dec A
 --else car9 when s="10000" 
 --else car11 when s="10010"
 --else car13 when s="10100"
-
 
 else '0';
 
@@ -155,7 +160,7 @@ else '0';
 								
 
 setf: f<=tmp_out;
-setflags: flags_out<=flags_tmp	;
+setflags: flags_out<=flags_tmp;
 
 
 END struct;
